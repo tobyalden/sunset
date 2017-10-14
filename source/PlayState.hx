87d6870d6ea3e706e1cf65c4faf0509cc0f3038d
Toby Alden <toby.alden@gmail.com>
2017-10-14 13:57:26 -0700

    Add scrolling backdrop

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,14 +1,21 @@
 package;
 
-import flixel.FlxState;
+import flixel.*;
+import flixel.addons.display.*;
 
 class PlayState extends FlxState
 {
+    public static inline var BACKDROP_SCROLL_SPEED = 200;
+
     private var player:Player;
+    private var backdrop:FlxBackdrop;
 
 	override public function create():Void
 	{
         player = new Player(20, 20);
+        backdrop = new FlxBackdrop('assets/images/backdrop.png');
+        backdrop.velocity.set(0, BACKDROP_SCROLL_SPEED);
+        add(backdrop);
         add(player);
 		super.create();
 	}
