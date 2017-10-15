a65da13368cf1d2e289ad7fefdf2310a7ea091a8
Toby Alden <toby.alden@gmail.com>
2017-10-15 14:00:13 -0700

    Fix indentation

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -14,8 +14,8 @@ class PlayState extends FlxState
     private var backdrop:FlxBackdrop;
     private var waveTimer:FlxTimer;
 
-	override public function create():Void
-	{
+    override public function create():Void
+    {
         player = new Player(20, 20);
         backdrop = new FlxBackdrop('assets/images/backdrop.png');
         backdrop.velocity.set(0, BACKDROP_SCROLL_SPEED);
@@ -23,8 +23,8 @@ class PlayState extends FlxState
         waveTimer.start(TIME_BETWEEN_WAVES, sendWave, 0);
         add(backdrop);
         add(player);
-		super.create();
-	}
+        super.create();
+    }
 
     override public function update(elapsed:Float):Void
     {
