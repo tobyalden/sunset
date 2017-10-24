4f09dde19447fa934bbab7d60792c88a64ae948e
Toby Alden <toby.alden@gmail.com>
2017-10-23 19:49:06 -0700

    Add limit to number of on screen enemies

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -21,18 +21,31 @@ class PlayState extends FlxState
         backdrop = new FlxBackdrop('assets/images/backdrop.png');
         backdrop.velocity.set(0, BACKDROP_SCROLL_SPEED);
         waveTimer = new FlxTimer();
+        // archer, crasher, creeper, demon, eye
+        // fighter, fossil, hydra, slither, star, turret
+        //waves = [
+            //new Wave(['fossil', 'fossil'], 7, player),
+            //new Wave(['crasher', 'crasher'], 5, player),
+            //new Wave(['fighter'], 10, player),
+            //new Wave(['demon'], 10, player),
+            //new Wave(['demon'], 10, player),
+            //new Wave(['eye', 'eye'], 7, player),
+            //new Wave(['archer', 'archer'], 7, player),
+            //new Wave(['hydra'], 5, player),
+            //new Wave(['slither'], 5, player),
+            //new Wave(['turret', 'turret', 'turret'], 5, player)
+        //];
         waves = [
-            new Wave(['archer', 'slither', 'slither'], 5, player),
-            //new Wave(['crasher', 'devil'], 5, player),
-            //new Wave(['creeper', 'demon', 'demon'], 6, player),
-            //new Wave(['demon', 'eye'], 5, player),
-            //new Wave(['eye', 'eye'], 5, player),
-            //new Wave(['fighter', 'fossil'], 6, player),
-            //new Wave(['fossil', 'hydra'], 4, player),
-            //new Wave(['hydra', 'star'], 6, player),
-            //new Wave(['slither', 'slither'], 5, player),
-            //new Wave(['star', 'archer', 'archer'], 6, player),
-            new Wave(['turret', 'turret', 'turret'], 5, player)
+            new Wave(['fossil', 'fossil', 'crasher', 'hydra'], 3, player),
+            new Wave(['crasher', 'crasher', 'turret', 'eye'], 5, player),
+            new Wave(['fighter', 'fighter'], 4, player),
+            new Wave(['demon', 'demon'], 2, player),
+            new Wave(['demon', 'hydra', 'creeper'], 4, player),
+            new Wave(['eye', 'slither', 'turret'], 4, player),
+            new Wave(['archer', 'archer', 'archer'], 4, player),
+            new Wave(['hydra', 'star', 'crasher'], 3, player),
+            new Wave(['slither', 'slither', 'slither'], 4, player),
+            new Wave(['turret', 'turret', 'turret', 'turret'], 4, player)
         ];
         new FlxRandom().shuffle(waves);
         add(backdrop);
@@ -45,6 +58,9 @@ class PlayState extends FlxState
     {
         if(Enemy.all.countLiving() <= 1 && waveTimer.timeLeft > 0.5) {
             waveTimer.reset(0.5);
+        }
+        if(Enemy.all.countLiving() > 6) {
+            waveTimer.reset(1);
         }
         // Damage enemies if they're touching
         FlxG.overlap(
